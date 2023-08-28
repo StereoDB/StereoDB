dbe8d416269e6d533b6b129715c5af7b97f13c62
antyadev <antyadev@gmail.com>
2023-08-28 13:14:16 +0300

    updated docs

---
+++ examples/Demo/Db.cs
@@ -0,0 +1,71 @@
+﻿namespace Demo;
+
+using StereoDB;
+using StereoDB.CSharp;
+
+// defines a Book type that implements IEntity<TId>
+public record Book : IEntity<int>
+{
+    public int Id { get; init; }
+    public string Title { get; init; }
+    public int Quantity { get; init; }
+}
+
+// defines an Order type that implements IEntity<TId>
+public record Order : IEntity<Guid>
+{
+    public Guid Id { get; init; }
+    public int BookId { get; init; }
+    public int Quantity { get; init; }
+}
+
+public record BooksSchema
+{
+    public ITable<int, Book> Table { get; init; }
+}
+
+public record OrdersSchema
+{
+    public ITable<Guid, Order> Table { get; init; }
+    public IValueIndex<int, Order> BookIdIndex { get; init; }
+}
+
+// defines a DB schema that includes Orders and Books tables
+// and a secondary index: 'BookIdIndex' for the Orders table
+public record Schema
+{
+    public BooksSchema Books { get; }
+    public OrdersSchema Orders { get; }
+    
+    public Schema()
+    {
+        Books = new BooksSchema
+        {
+            Table = StereoDbEngine.CreateTable<int, Book>()
+        };
+
+        var ordersTable = StereoDbEngine.CreateTable<Guid, Order>();
+
+
+        Orders = new OrdersSchema()
+        {
+            Table = ordersTable,
+            BookIdIndex = ordersTable.AddValueIndex(order => order.BookId)
+        };
+    }
+}
+
+// defines a DB that implements IStereoDb<Schema>
+public class Db : IStereoDb<Schema>
+{
+    private readonly StereoDbEngine<Schema> _engine = StereoDbEngine.Create(new Schema());
+
+    public T ReadTransaction<T>(Func<ReadOnlyTsContext<Schema>, T> transaction) => 
+        _engine.ReadTransaction(transaction);
+    
+    public T WriteTransaction<T>(Func<ReadWriteTsContext<Schema>, T> transaction) => 
+        _engine.WriteTransaction(transaction);
+
+    public void WriteTransaction(Action<ReadWriteTsContext<Schema>> transaction) =>
+        _engine.WriteTransaction(transaction);
+}
---
+++ examples/Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using Demo;
+
+var db = new Db();
+
+// 1) adds book
+// WriteTransaction: it's a read-write transaction: we can query and mutate data
+
+db.WriteTransaction(ctx =>
+{
+    var books = ctx.UseTable(ctx.Schema.Books.Table);
+
+    foreach (var id in Enumerable.Range(0, 10))
+    {
+        var book = new Book {Id = id, Title = $"book_{id}", Quantity = 1};
+        books.Set(book);
+    }
+});
+       
+// 2) creates an order
+// WriteTransaction: it's a read-write transaction: we can query and mutate data
+
+db.WriteTransaction(ctx =>
+{
+    var books = ctx.UseTable(ctx.Schema.Books.Table);
+    var orders = ctx.UseTable(ctx.Schema.Orders.Table);
+
+    foreach (var id in books.GetIds())
+    {
+        if (books.TryGet(id, out var book) && book.Quantity > 0)
+        {
+            var order = new Order {Id = Guid.NewGuid(), BookId = id, Quantity = 1};
+            var updatedBook = book with { Quantity = book.Quantity - 1 };
+            
+            books.Set(updatedBook);
+            orders.Set(order);
+        }
+    }
+});
+        
+// 3) query book and orders
+// ReadTransaction: it's a read-only transaction: we can query multiple tables at once
+
+var result = db.ReadTransaction(ctx =>
+{
+    var books = ctx.UseTable(ctx.Schema.Books.Table);
+    var bookIdIndex = ctx.Schema.Orders.BookIdIndex;
+
+    if (books.TryGet(1, out var book))
+    {
+        var orders = bookIdIndex.Find(book.Id).ToArray();
+        return (book, orders);
+    }
+    
+    return (null, null);
+});
