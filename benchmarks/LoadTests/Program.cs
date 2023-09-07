c66e1c73cf916bf56b66b55e2d9ee19f36a740d3
Anton Moldovan <antyadev@gmail.com>
2023-09-07 14:15:20 +0200

    added redis benchmark (#6)
    * added redis benchmark

---
+++ benchmarks/LoadTests/Program.cs
@@ -0,0 +1,5 @@
+﻿using LoadTests.Redis;
+
+RedisLoadTest.Run(args);
+
+Task.Delay(-1).Wait();
---
+++ benchmarks/LoadTests/Redis/RedisLoadTest.cs
@@ -0,0 +1,18 @@
+﻿using LoadTests.Redis.Scenarios;
+using NBomber.CSharp;
+
+namespace LoadTests.Redis;
+
+public class RedisLoadTest
+{
+    public static void Run(string[] args)
+    {
+        NBomberRunner.RegisterScenarios(
+            new RedisInitScenario().Create(),
+            new RedisReadScenario().Create(),
+            new RedisWriteScenario().Create()
+        )
+        //.LoadConfig("./Redis/Configs/swarm-nbomber-config.json")
+        .Run(args);
+    }
+}
