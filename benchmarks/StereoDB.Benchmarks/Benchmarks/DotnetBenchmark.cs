2318486ceb4de6944c7f1b3645884db0dd889f36
antyadev <antyadev@gmail.com>
2023-09-15 18:59:25 +0300

    added DotnetBenchmark.cs

---
+++ benchmarks/StereoDB.Benchmarks/Benchmarks/DotnetBenchmark.cs
@@ -0,0 +1,84 @@
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+
+namespace StereoDB.Benchmarks.Benchmarks;
+
+//[MemoryDiagnoser]
+[AllStatisticsColumn] // RankColumn, MinColumn, MaxColumn, Q1Column, Q3Column, AllStatisticsColumn 
+public class DotnetBenchmark
+{
+    private const int Iterations = 1000;
+    
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private int Add(int a, int b) => a + b;
+    
+    [Benchmark(OperationsPerInvoke = Iterations)]
+    public void SimpleMethod()
+    {
+        var k = 0;
+        
+        for (int i = 0; i < Iterations; i++)
+        {
+            k = Add(i, i);
+        }
+    }
+
+    private ValueTask<int> AddValueAsync(int a, int b) => ValueTask.FromResult(a + b);
+    
+    [Benchmark(OperationsPerInvoke = Iterations)]
+    public async ValueTask SimpleMethodValueAsync()
+    {
+        var k = 1;
+        for (int i = 0; i < Iterations; i++)
+        {
+            k = await AddValueAsync(i, i);
+        }
+    }
+    
+    private Task<int> AddAsync(int a, int b) => Task.FromResult(a + b);
+    
+    [Benchmark(OperationsPerInvoke = Iterations)]
+    public async Task SimpleMethodAsync()
+    {
+        var k = 0;
+        
+        for (int i = 0; i < Iterations; i++)
+        {
+            k = await AddAsync(i, i);
+        }
+    }
+    
+    [Benchmark(OperationsPerInvoke = Iterations)]
+    public async Task SimpleMethodAsyncYield()
+    {
+        var k = 0;
+        
+        for (int i = 0; i < Iterations; i++)
+        {
+            k = await AddAsyncYield(i, i);
+        }
+    }
+    
+    private async Task<int> AddAsyncYield(int a, int b)
+    {
+        await Task.Yield();
+        return a + b;
+    }
+    
+    [Benchmark(OperationsPerInvoke = Iterations)]
+    public async Task SimpleMethodAsync2()
+    {
+        var k = 0;
+        
+        for (int i = 0; i < Iterations; i++)
+        {
+            k = await AddAsync2(i, i);
+        }
+    }
+    
+    private async Task<int> AddAsync2(int a, int b)
+    {
+        await Task.Yield();
+        return await Task.Run(() => a + b);
+    }
+}
--- benchmarks/StereoDB.Benchmarks/Program.cs
+++ benchmarks/StereoDB.Benchmarks/Program.cs
@@ -3,7 +3,9 @@ using BenchmarkDotNet.Running;
 using StereoDB.Benchmarks.Benchmarks;
 
 //BenchmarkRunner.Run<StereoDbBenchmark>(new DebugInProcessConfig());
-//BenchmarkRunner.Run<StereoDbBenchmark>();
+BenchmarkRunner.Run<StereoDbBenchmark>();
 
-BenchmarkRunner.Run<LMDBBenchmark>();
+//BenchmarkRunner.Run<LMDBBenchmark>();
 //BenchmarkRunner.Run<LMDBBenchmark>(new DebugInProcessConfig());
+
+//BenchmarkRunner.Run<DotnetBenchmark>(args: args);
