24a30191de80a2e41b59f6bb404314a7e419b531
Andrii Kurdiumov <kant2002@gmail.com>
2023-11-26 00:10:15 +0600

    Add table alias support for select

--- tests/StereoDB.Tests/Sql/Select.fs
+++ tests/StereoDB.Tests/Sql/Select.fs
@@ -11,6 +11,12 @@ type SubBook =
         Quantity: int
     }
 
+type SuperBook =
+    {
+        SuperId: int
+        Quantity: int
+    }
+
 [<Fact>]
 let ``Select all rows`` () =
     let db = StereoDb.create(Schema())
@@ -246,4 +252,33 @@ let ``TOP`` () =
     let book2 = result.Value[2]
     test <@ book2.Id = 2 @>
     test <@ book2.Quantity = 4 @>
+    
 
+[<Theory>]
+[<InlineData("SELECT b.Id as SuperId, b.Quantity FROM Books AS b ORDER BY Quantity, Title")>]
+[<InlineData("SELECT b.Id as SuperId, b.Quantity FROM Books b ORDER BY Quantity, Title")>]
+let ``Alias for table`` (sql) =
+    let db = StereoDb.create(Schema())
+    
+    // add books
+    db.WriteTransaction(fun ctx ->
+        let books = ctx.UseTable(ctx.Schema.Books.Table)
+        
+        for i in [1..10] do
+            let book = { Id = i; Title = $"book_{i}"; Quantity = abs(6 - i) }
+            books.Set book
+    )
+
+    let result = db.ExecSql<SuperBook> sql
+    
+    let booksCount = result.Value.Count
+    test <@ booksCount = 10 @>
+    let book1 = result.Value[0]
+    test <@ book1.SuperId = 6 @>
+    test <@ book1.Quantity = 0 @>
+    let book2 = result.Value[1]
+    test <@ book2.SuperId = 5 @>
+    test <@ book2.Quantity = 1 @>
+    let book2 = result.Value[2]
+    test <@ book2.SuperId = 7 @>
+    test <@ book2.Quantity = 1 @>
