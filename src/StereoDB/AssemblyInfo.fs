9ac25e4beedd33523bbe753297443ad78ae6a6e7
antyadev <antyadev@gmail.com>
2024-12-16 20:42:03 +0200

    moved Sql to StereoDb.Sql repository

--- src/StereoDB/AssemblyInfo.fs
+++ src/StereoDB/AssemblyInfo.fs
@@ -3,5 +3,8 @@
 open System.Runtime.CompilerServices
 
 [<assembly: InternalsVisibleTo("StereoDB.Tests")>]
+[<assembly: InternalsVisibleTo("StereoDB.Sql")>]
+[<assembly: InternalsVisibleTo("StereoDB.Sql.Tests")>]
+
 do()
 
