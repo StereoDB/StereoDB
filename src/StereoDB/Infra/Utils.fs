bbf2fd477ee0cc9546e51cbb1ca0f6a80692db00
antyadev <antyadev@gmail.com>
2024-12-26 11:17:45 +0200

    cleanup

--- src/StereoDB/Infra/Utils.fs
+++ src/StereoDB/Infra/Utils.fs
@@ -1,9 +1,7 @@
 ﻿module internal StereoDB.Infra.Utils
 
 open System
-open System.Linq
 open System.Collections.Generic
-open System.Net.Sockets
 open MessagePack
 open MessagePack.Resolvers
 
@@ -51,11 +49,3 @@ module MessagePack =
     
     let defaultOptions =
         MessagePackSerializerOptions.Standard.WithResolver(ContractlessStandardResolverAllowPrivate.Instance)
-
-
-module IPAddress = 
-    open System.Net
-
-    let nodeIp = 
-        let ipHostInfo = Dns.GetHostEntry(Dns.GetHostName())
-        ipHostInfo.AddressList.First(fun a -> a.AddressFamily = AddressFamily.InterNetwork)
---
+++ src/StereoDB/StereoDbSettings.fs
@@ -0,0 +1,29 @@
+﻿namespace StereoDB
+
+open System.Runtime.InteropServices
+open MessagePack
+
+type StereoDbSettings = {    
+    LocalPersistenceEnabled: bool
+    EntitySerializer: IEntitySerializer
+    DbFolderPath: string
+}
+with
+    static member OnlyInMemory = {        
+        LocalPersistenceEnabled = false
+        EntitySerializer = Unchecked.defaultof<_>
+        DbFolderPath = ""        
+    }
+    
+    static member FileStorageMsgPack(
+        [<Optional; DefaultParameterValue(null:MessagePackSerializerOptions)>] options: MessagePackSerializerOptions,
+        [<Optional; DefaultParameterValue("":string)>] dbFolderPath: string) = {
+        
+        LocalPersistenceEnabled = true
+        DbFolderPath = dbFolderPath
+        EntitySerializer = {
+            new IEntitySerializer with
+                member this.Serialize(writer, value) = MessagePackSerializer.Serialize(writer, value, options)
+                member this.Deserialize(data) = MessagePackSerializer.Deserialize(data, options)
+        }        
+    }
